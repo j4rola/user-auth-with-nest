dca9cdf96b41627f585edd310108b3c420f2c26a
Joe Froula <joefroula@Users-MacBook-Pro.local>
2021-11-10 08:47:03 -0800

    initial

---
+++ src/database/migrations/20211109224620-create-user.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        unique: true,
+        primaryKey: true,
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      userName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      passWord: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
---
+++ src/user/user.model.ts
@@ -0,0 +1,18 @@
+import { Column, Model, Table } from 'sequelize-typescript';
+
+@Table
+export class User extends Model<User> {
+  @Column
+  firstName: string;
+
+  @Column
+  lastName: string;
+
+  @Column
+  userName: string;
+
+  @Column
+  passWord: string;
+  
+
+}
