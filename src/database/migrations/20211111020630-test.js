fcc8fea211d11e3d4ca67b5a3f9610b504e7a55d
Joe Froula <joefroula@Users-MacBook-Pro.local>
2021-11-10 18:35:51 -0800

    final commit

---
+++ src/database/migrations/20211111020630-test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(t => {
+      return Promise.all([
+        queryInterface.addColumn('Users', 'Test Col', {
+          type: Sequelize.DataTypes.STRING
+        }, { transaction: t }),
+      ]);
+    });
+  },
+
+  //
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
---
+++ src/database/migrations/20211111021513-test2.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(t => {
+      return Promise.all([
+        queryInterface.addColumn('Users', 'Test Col1', {
+          type: Sequelize.DataTypes.STRING
+        }, { transaction: t }),
+      ]);
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
