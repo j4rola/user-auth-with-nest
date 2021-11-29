3d57a0175c4088670bfad1250d69fe4193b72ce1
Josef Froula <47036843+j4rola@users.noreply.github.com>
2021-11-29 07:54:47 -0800

    Update post.service.ts

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import Ctx from 'src/types/context.type'; 
-//import { PostInput } from 'src/graphql';
 import { Post } from '../graphql'
 import { PostEntity } from './post.model';
 import { InjectModel } from '@nestjs/sequelize';
@@ -16,15 +15,17 @@ export class PostService {
         private postModel: typeof PostEntity, 
         ) {} 
     
-        async createPost(args: Post, context: Ctx): Promise<PostEntity> {
+        async createPost(args: Post, context: Ctx): Promise<PostEntity> { 
     
             let userData: Partial <PostEntity> = {post: args.post, userKey: context.req.user.userKey} 
+            
+            /* Context allows us to access information about the currently logged in user. The arguments passed to our 'createPost' function are the Post type 
+               rom our graphql schema and the user context. This allows us to create a post using the userKey from the currently logged in user, and the 
+               template that we have defined for a new post in our schema. */ 
     
     
             let createdPost = await this.postModel.create(userData as any)  
-    
-            //createdPost.userKey = context.req.header.arguments.userKey
-    
+        
             return createdPost 
         }
 
